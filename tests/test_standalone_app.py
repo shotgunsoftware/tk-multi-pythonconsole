07f3c94f66c46a3b78830d9c45be1a7714b2bf5a
Philip Scadding <philip.scadding@autodesk.com>
2020-02-25 18:18:14 +0000

    Added a test for the comment at the end of the line.

--- tests/test_standalone_app.py
+++ tests/test_standalone_app.py
@@ -210,6 +210,8 @@ def test_execute_script(
         ("    a = 1", "    a\n    ", "new_line", (5, 9)),
         # test adding a new line after a colon, it should auto indent by 4
         ("    def test(): ", "    def test(): \n        ", "new_line", (16, 16)),
+        # test adding a new line after a colon with an inline comment, it should auto indent by 4
+        ("def test(): # asd#.", "def test(): # asd#.\n    ", "new_line", (19, 19)),
         # test adding a new line with the selection before the colon, it should indent to the same level.
         ("    def test(): ", "    def test(\n    ): ", "new_line", (13, 13)),
     ],
