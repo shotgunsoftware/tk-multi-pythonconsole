98a1f3991e657080e76109763669fa6266930581
mathurf <francis.mathurin@autodesk.com>
2021-01-12 14:21:54 -0500

    Now using create_unique_name method

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -15,6 +15,7 @@ import os
 import sys
 import sgtk
 from tk_toolchain.authentication import get_toolkit_user
+from tk_toolchain.testing import create_unique_name
 
 try:
     from MA.UI import topwindows
@@ -38,10 +39,7 @@ def sg_project(shotgun):
     Generates a fresh Shotgun Project to use with the Shotgun Python Console UI Automation.
     """
     # Make sure there is not already an automation project created
-    project_name = (
-        "Toolkit Python Console UI Automation "
-        + os.environ["SHOTGUN_TEST_ENTITY_SUFFIX"]
-    )
+    project_name = create_unique_name("Toolkit Python Console UI Automation-")
     filters = [["name", "is", project_name]]
     existed_project = shotgun.find_one("Project", filters)
     if existed_project is not None:
